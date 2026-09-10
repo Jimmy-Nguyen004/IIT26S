d7ff76734121c17c5b7bb13ff1de237246be56f7
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-10 18:17:00 +0300

    Upadate files

---
+++ self_study.py
@@ -0,0 +1,33 @@
+# Variable = A container for storing data values (string, integer, float, boolean, etc.)
+#            A varible behaves as if it was the value that it contains.
+
+#Strings
+first_name = "Khiem"
+food = "Pizza"
+email = "khiem@example.com" 
+
+#Integers
+age = 22
+quantity = 5
+num_of_students = 30
+
+#Floats
+price = 10.99
+gpa = 3.8
+distance = 10
+
+#Booleans
+is_student = True
+for_sale = False
+
+#      print(f"My name is {first_name}")
+#      print(f"I am {age} years old")
+#      print(f"My email is {email}")
+#      print(f"The price of this one is {price} euros")
+
+#      if is_student:
+#          print("I am a student")
+
+# Typecasting = Converting a value from one data type to another data type
+#              (float, int, str, etc.)
+#              str(), int(), float(), bool()
