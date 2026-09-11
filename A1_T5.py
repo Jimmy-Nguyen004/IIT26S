1fbea35d620536da4c52e5f7118f60fa28c06926
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-11 19:11:51 +0300

    done t6

---
+++ A1_T5.py
@@ -0,0 +1,13 @@
+print("Calculate the area of a wall.")
+
+value1 = input("Enter the width in meters: ")
+Width = int(value1)
+
+value2 = input("Enter the height in meters: ")
+Height = int(value2)
+
+print(f"Width is {Width} m and height is {Height} m.")
+
+Area = Width * Height
+
+print(f"The wall will be {Area} square meters.")
