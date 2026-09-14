26c7da3cb229460f6810e550bf04803e3715a3f3
Nguyen Cat Duc Khiem <duckhiem373@gmail.com>
2026-09-14 10:58:58 +0300

    changes

--- week_1/A1_T2.py
+++ week_1/A1_T2.py
@@ -4,6 +4,6 @@ print(Name1)
 Name2 = 'Harry'
 print(Name2)
 
-print(f'{Name1}, {Name2}')
+print(f'{Name1} {Name2}')
 print(f'{Name1} is eating ice cream with {Name2}')
 print(f"{Name1} and {Name2} are friends")
